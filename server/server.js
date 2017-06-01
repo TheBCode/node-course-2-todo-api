296791f12007fb25c5207502b7869f260265d5a0
Byron Buyck <bbuyck1@gmail.com>
2017-05-31 21:32:49 -0400

    Learn to use Mongoose by creating schemas and validation

---
+++ server/server.js
@@ -0,0 +1,51 @@
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost:27017/TodoApp');
+
+var Todo = mongoose.model('Todo', {
+   text: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+   },
+    completed: {
+    type: Boolean,
+    default: false
+    },
+    completedAt: {
+    type: Number,
+    default: null
+    }
+});
+
+//
+// var otherTodo = new Todo({
+//     text: 'Something else'
+// });
+//
+// otherTodo.save().then((doc) => {
+//     console.log('Saved todo', doc);
+// }, (e) =>{
+//     console.log('Unable to save Todo');
+// });
+
+var User = mongoose.model('User', {
+    email: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
+    }
+});
+
+var newUser = new User({
+   email: ' bbuyck1@example.com   '
+});
+
+newUser.save().then((doc) => {
+    console.log('Saved user', doc);
+}, (e) =>{
+    console.log('Invalid User - Try again')
+});
