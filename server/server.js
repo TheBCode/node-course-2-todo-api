f431163a2df9572c86de1142a925addbbc479f23
Byron Buyck <bbuyck1@gmail.com>
2017-06-09 12:54:02 -0400

    Setup separate test and dev envs

--- server/server.js
+++ server/server.js
@@ -1,15 +1,17 @@
+require('./config/config');
+
+
 const _ = require('lodash');
-var express = require('express');
-var bodyParser = require('body-parser');
-var {ObjectID} = require('mongodb');
+const express = require('express');
+const bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
-const port = process.env.PORT || 3000;
-
+const port = process.env.PORT;
 
 app.use(bodyParser.json());
 
@@ -20,20 +22,19 @@ app.post('/todos', (req, res) => {
 
     todo.save().then((doc) => {
         res.send(doc);
-    }, (e) =>{
+    }, (e) => {
         res.status(400).send(e);
     });
 });
 
 app.get('/todos', (req, res) => {
-   Todo.find().then((todos) => {
-    res.send({todos});
-   }, (e) => {
-    res.status(400). send(e);
-   });
+    Todo.find().then((todos) => {
+        res.send({todos});
+    }, (e) => {
+        res.status(400).send(e);
+    });
 });
 
-// GET /todos/1234324
 app.get('/todos/:id', (req, res) => {
     var id = req.params.id;
 
@@ -45,14 +46,15 @@ app.get('/todos/:id', (req, res) => {
         if (!todo) {
             return res.status(404).send();
         }
+
         res.send({todo});
     }).catch((e) => {
         res.status(400).send();
     });
 });
 
-app.delete('/todos/:id', (req, res) =>{
-   var id = req.params.id;
+app.delete('/todos/:id', (req, res) => {
+    var id = req.params.id;
 
     if (!ObjectID.isValid(id)) {
         return res.status(404).send();
@@ -62,6 +64,7 @@ app.delete('/todos/:id', (req, res) =>{
         if (!todo) {
             return res.status(404).send();
         }
+
         res.send({todo});
     }).catch((e) => {
         res.status(400).send();
@@ -94,10 +97,8 @@ app.patch('/todos/:id', (req, res) => {
     })
 });
 
-
-    app.listen(port, () => {
-        console.log(`Started on port ${port}`);
-    });
-
+app.listen(port, () => {
+    console.log(`Started up at port ${port}`);
+});
 
 module.exports = {app};
