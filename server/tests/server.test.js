fe0ee4cfa3c64a75719f0d2e397bfd53242356c6
Byron Buyck <bbuyck1@gmail.com>
2017-07-11 18:55:29 -0400

    Add test for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -221,7 +221,7 @@ describe('POST /users', () => {
     });
 });
 
-describe('POST /users/login', () =>{
+describe('POST /users/login', () => {
     it('should login user and return auth token', (done) => {
         request(app)
             .post('/users/login')
@@ -269,5 +269,24 @@ describe('POST /users/login', () =>{
                     done();
                 }).catch((e) => done(e));
             });
+    });
+});
+
+describe('DELETE /users/me/token', () => {
+    it('should remove auth token on logout', (done) => {
+       request(app)
+           .delete('/users/me/token')
+           .set('x-auth', users[0].tokens[0].token)
+           .expect(200)
+           .end((err, res) => {
+            if (err) {
+                return done(err);
+            }
+
+            User.findById(users[0]._id).then((user) => {
+                expect(user.tokens.length).toBe(0);
+                done();
+            }).catch((e) => done(e));
+           });
     });
 });
